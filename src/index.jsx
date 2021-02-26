c78237bf6571201e2bad920a9a99a6c1278df999
Marcio Samuel <marciosamuel12@outlook.com>
2021-02-26 19:40:16 -0300

    remoção da pasta e componente main, colocando a aplicação principal dentro do index.js | estilização base da aplicação

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Portfolio from './main/Portfolio';
+import Home from './components/Home';
 
 library.add(fab, far, fas);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Portfolio />
+    <Home id="home" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
